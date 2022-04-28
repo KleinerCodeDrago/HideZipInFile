70f8ab1551743327b1f7b7d6a20908325b74a980
KleinerCodeDrago <71352143+KleinerCodeDrago@users.noreply.github.com>
2022-04-28 14:18:37 +0200

    Should work, but does not

--- HideZipInFile/HideZipInFile/Form1.Designer.cs
+++ HideZipInFile/HideZipInFile/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.ChooseZipLabel = new System.Windows.Forms.Label();
             this.fileHideInLabel = new System.Windows.Forms.Label();
+            this.CombineButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ChooseZipLabel
@@ -38,7 +39,7 @@
             this.ChooseZipLabel.AutoSize = true;
             this.ChooseZipLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ChooseZipLabel.ForeColor = System.Drawing.Color.Red;
-            this.ChooseZipLabel.Location = new System.Drawing.Point(53, 59);
+            this.ChooseZipLabel.Location = new System.Drawing.Point(138, 102);
             this.ChooseZipLabel.Name = "ChooseZipLabel";
             this.ChooseZipLabel.Size = new System.Drawing.Size(420, 54);
             this.ChooseZipLabel.TabIndex = 0;
@@ -52,7 +53,7 @@
             this.fileHideInLabel.AutoSize = true;
             this.fileHideInLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.fileHideInLabel.ForeColor = System.Drawing.Color.Red;
-            this.fileHideInLabel.Location = new System.Drawing.Point(750, 59);
+            this.fileHideInLabel.Location = new System.Drawing.Point(738, 111);
             this.fileHideInLabel.Name = "fileHideInLabel";
             this.fileHideInLabel.Size = new System.Drawing.Size(401, 54);
             this.fileHideInLabel.TabIndex = 1;
@@ -60,11 +61,22 @@
             this.fileHideInLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.fileHideInLabel.Click += new System.EventHandler(this.fileHideInLabel_Click);
             // 
+            // CombineButton
+            // 
+            this.CombineButton.Location = new System.Drawing.Point(453, 282);
+            this.CombineButton.Name = "CombineButton";
+            this.CombineButton.Size = new System.Drawing.Size(354, 137);
+            this.CombineButton.TabIndex = 2;
+            this.CombineButton.Text = "Click to combine";
+            this.CombineButton.UseVisualStyleBackColor = true;
+            this.CombineButton.Click += new System.EventHandler(this.CombineButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1348, 775);
+            this.Controls.Add(this.CombineButton);
             this.Controls.Add(this.fileHideInLabel);
             this.Controls.Add(this.ChooseZipLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -82,5 +94,6 @@
 
         private Label ChooseZipLabel;
         private Label fileHideInLabel;
+        private Button CombineButton;
     }
 }
--- HideZipInFile/HideZipInFile/Form1.cs
+++ HideZipInFile/HideZipInFile/Form1.cs
@@ -12,9 +12,11 @@ namespace HideZipInFile
         {
             ChooseZipLabel.Location = new Point(((Size.Width / 4) - ChooseZipLabel.Width / 2), ((Size.Height / 10) * 3) - ChooseZipLabel.Size.Height / 2);
             fileHideInLabel.Location = new Point((((Size.Width / 4) * 3) - fileHideInLabel.Width / 2), ((Size.Height / 10) * 3) - fileHideInLabel.Size.Height / 2);
-            MinimumSize = new Size(ChooseZipLabel.Width + fileHideInLabel.Width, Math.Max(ChooseZipLabel.Height, fileHideInLabel.Height)+100);
+            CombineButton.Location = new Point(((Size.Width / 2)) - CombineButton.Width / 2, (Size.Height / 10)*6 - CombineButton.Height / 2);
+            MinimumSize = new Size(ChooseZipLabel.Width + fileHideInLabel.Width, Math.Max(ChooseZipLabel.Height, fileHideInLabel.Height)+CombineButton.Height+100);
         }
-
+        string PathToHideIn = "";
+        string HideFilePath = "";
         private void ChooseZipLabel_Click(object sender, EventArgs e)
         {
             var filePath = string.Empty;
@@ -37,8 +39,9 @@ namespace HideZipInFile
                 }
             }
 
-            if (filePath != null)
+            if (filePath != "")
             {
+                HideFilePath = filePath;
                 ChooseZipLabel.ForeColor = Color.Green;
             }
         }
@@ -63,9 +66,9 @@ namespace HideZipInFile
 
                 }
             }
-
-            if (filePath != null)
+            if (filePath != "")
             {
+                PathToHideIn = filePath;
                 fileHideInLabel.ForeColor = Color.Green;
             }
         }
@@ -73,8 +76,19 @@ namespace HideZipInFile
         private void Form1_Resize(object sender, EventArgs e)
         {
             Debug.WriteLine(Size);
+            CombineButton.Location = new Point(((Size.Width / 2)) - CombineButton.Width / 2, (Size.Height / 10) * 6 - CombineButton.Height / 2);
             ChooseZipLabel.Location = new Point(((Size.Width / 4)- ChooseZipLabel.Width/2), ((Size.Height / 10)*3)-ChooseZipLabel.Size.Height/2);
             fileHideInLabel.Location = new Point((((Size.Width / 4)*3)- fileHideInLabel.Width/2), ((Size.Height / 10)*3)-fileHideInLabel.Size.Height/2);
+        }
+
+        private void CombineButton_Click(object sender, EventArgs e)
+        {
+            if (PathToHideIn != "" && HideFilePath != "")
+            {
+                string strCmdText;
+                strCmdText = $"copy /b {PathToHideIn.Replace(@"\\", @"\")}+{HideFilePath.Replace(@"\\", @"\")} output.png";
+                System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            }
         }
     }
 }
